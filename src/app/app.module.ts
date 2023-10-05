3e7acb1c42a62b5fc1eb995cce5d6303c2033d8c
Rinaudo-Lorenzo <l.rinaudo.2301@vallauri.edu>
2023-10-05 10:14:19 +0200

    Implementato component "neve"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DadiComponent } from './dadi/dadi.component';
+import { NeveComponent } from './neve/neve.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DadiComponent
+    DadiComponent,
+    NeveComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/neve/neve.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-neve',
+  templateUrl: './neve.component.html',
+  styleUrls: ['./neve.component.css']
+})
+export class NeveComponent {
+
+}
